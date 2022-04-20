8925dafbc4bdca0eac134cbd93cbea686e8a6795
Su <su.dong1@outlook.com>
2022-04-20 07:06:14 -0500

    added Navigation

--- qs_chat/src/Componts/Navigation/Navigation.js
+++ qs_chat/src/Componts/Navigation/Navigation.js
@@ -1,14 +1,52 @@
 import React from "react";
 import logo from "./logo.png";
+import "./Navigation.css";
 
 
-const Navigation = () =>{
-    return(        
-        <nav style = {{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
-        </nav>
+const Navigation = (page) =>{
     
-    );
+    
+    switch(page.page){
+
+        case "home":
+            return(        
+                <nav className="dt w-100 border-box pa3 ph5-ns">
+                    <a className="dtc v-mid mid-gray link dim w-25" href="#" title="Home">
+                        <img src={logo} className="dib w4 h4 br-100" alt="Site Name"/>
+                    </a>
+                    <div className="dtc v-mid w-75 tr">
+                        <a className="f3 link dim black underline pa3 pointer" href="#" title="Register">Register</a>
+                        <a className="f3 link dim black underline pa3 pointer" href="#" title="Sign in">Sign In</a>
+                    </div>
+                </nav>
+            
+            );
+        case "chatting":
+            return(        
+                <nav className="dt w-100 border-box pa3 ph5-ns">
+                    <a className="dtc v-mid mid-gray link dim w-25" href="#" title="Home">
+                        <img src={logo} className="dib w4 h4 br-100" alt="Site Name"/>
+                    </a>
+                    <div className="dtc v-mid w-75 tr">
+                        <a className="f3 link dim black underline pa3 pointer" href="#" title="Sign Out">Sign Out</a>
+                    </div>
+                </nav>
+            
+            );
+        case "register":
+            return(        
+                <nav className="dt w-100 border-box pa3 ph5-ns">
+                    <a className="dtc v-mid mid-gray link dim w-25" href="#" title="Home">
+                        <img src={logo} className="dib w4 h4 br-100" alt="Site Name"/>
+                    </a>
+                    <div className="dtc v-mid w-75 tr">
+                        <a className="f3 link dim black underline pa3 pointer" href="#" title="Sign in">Sign In</a>
+                        <a className="f3 link dim black underline pa3 pointer" href="#" title="Sign Out">Sign Out</a>
+                    </div>
+                </nav>
+            
+            );
+    }
 }
 
 export default Navigation
--- qs_chat/src/index.js
+++ qs_chat/src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Navigation from './Componts/Navigation/Navigation';
-import Logo from './Componts/Logo/Logo';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navigation />
-    <Logo/>
+    <App/>
   </React.StrictMode>
 );
 
